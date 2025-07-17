53bc57569c7b183d2c4e536544a47d50be0346c0
Reza Ashena <Ashena.reza@gmail.com>
2025-07-17 14:22:24 -0400

    Add solar panel output prediction model
    Removed the "Hello, World!" message and implemented a model for predicting solar panel output using linear regression. Introduced example data for sunshine hours and revenue. Added two classes: `SunData` for storing sunshine hours and revenue, and `RevenuePrediction` for predicted revenue.

--- ML_02_Solar_Pannel_Prediction_LR/Program.cs
+++ ML_02_Solar_Pannel_Prediction_LR/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Create a model for predicting solar panel output using linear regression, relation between sunshine hours and revenue during the day.
+
+//example data
+var data = new[]
+{
+    new SunData { SunHours = 1, Revenue = 100 },
+    new SunData { SunHours = 2, Revenue = 120 },
+    new SunData { SunHours = 3, Revenue = 140 },
+    new SunData { SunHours = 4, Revenue = 160 },
+    new SunData { SunHours = 5, Revenue = 180 }
+};
+#region classes
+
+
+public class SunData
+{
+    public float SunHours { get; set; } //hours of sunshine during the day
+    public float Revenue { get; set; } //gains from solar panel during the day
+}
+
+public class RevenuePrediction
+{
+    public float Score { get; set; } //predicted revenue from solar panel
+}
+#endregion
